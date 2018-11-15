994cf55bfa6288f0c407fa407bef4eda467780f0
Andrey Galkin <andrey@futoin.org>
2018-11-15 23:48:22 +0200

    Minor revert to keep using xenial builds for Debian

--- manifests/debian/params.pp
+++ manifests/debian/params.pp
@@ -6,9 +6,11 @@
 # Please see README
 class cffirehol::debian::params {
     if ($::facts['operatingsystem'] == 'Debian' and
-            versioncmp($::facts['operatingsystemrelease'], '9') >= 0) or
-        ($::facts['operatingsystem'] == 'Ubuntu' and
-            versioncmp($::facts['operatingsystemrelease'], '18.04') >= 0)
+            versioncmp($::facts['operatingsystemrelease'], '9') >= 0)
+    {
+        $launchpad_release = 'xenial'
+    } elsif ($::facts['operatingsystem'] == 'Ubuntu' and
+             versioncmp($::facts['operatingsystemrelease'], '18.04') >= 0)
     {
         $launchpad_release = 'bionic'
     } else {
